40859d214a9cc467ea40985bda2b7641ea1fb1f3
Sune Simonsen <sune@we-knowhow.dk>
2017-09-09 20:59:51 +0200

    Initial setup

---
+++ button.js
@@ -0,0 +1,53 @@
+import { html, render } from './node_modules/lit-html/lib/lit-extended.js'
+
+export class Button extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    render(this.render(), this.shadowRoot);
+  }
+
+  render() {
+    return html`
+      <style>
+        button {
+          transition: border-color .25s ease-in-out,box-shadow .1s ease-in-out,background-color .25s ease-in-out,color .25s ease-in-out;
+          border: 1px solid #30aabc;
+          border-radius: 4px;
+          background-color: transparent;
+          cursor: pointer;
+          padding: 0.6em 2.25em;
+          text-align: center;
+          color: #30aabc;
+          font-size: 12px;
+          font-weight: 400;
+          box-sizing: border-box;
+          user-select: none;
+        }
+
+        button:hover {
+          border-color: transparent;
+          background-color: #41c8dc;
+          color: #fff;
+        }
+
+        button:focus {
+          outline: none;
+          box-shadow: 0 0 0 3px rgba(48,170,188,.4);
+        }
+
+        button:active {
+          transition: border-color .1s ease-in-out,background-color .1s ease-in-out,color .1s ease-in-out;
+          border-color: transparent;
+          background-color: #3094a3;
+          text-decoration: none;
+          color: #fff;
+          box-shadow: none;
+        }
+      </style>
+      <button><slot></slot></button>
+    `;
+  }
+}
+
+customElements.define('z-button', Button);
---
+++ index.js
@@ -0,0 +1,72 @@
+import './button.js'
+import './callout.js'
+
+import { html, render } from './node_modules/lit-html/lib/lit-extended.js'
+
+const handler = (e) => {
+  alert('Click!')
+}
+
+
+const indexTemplate = (text) => html`
+  <style>
+    html {
+      font-family: system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Oxygen-Sans,Arial,sans-serif;
+      font-size: 14px;
+    }
+
+    button {
+      background: darkred;
+      color: white;
+      border: thin solid red;
+    }
+
+
+    section > h1 { font-size: 18px; }
+    section > p { margin: 8px; }
+
+    .callouts {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-gap: 20px;
+      margin: 20px;
+    }
+  </style>
+  <section>
+    <h1>Buttons</h1>
+    <p>This is a custom button: <z-button onclick=${handler}><strong>${text}</strong></z-button></p>
+    <p>This is another custom button: <z-button onclick=${handler}>Another ${text}</z-button></p>
+    <p>This is a normal button: <button onclick=${handler}><strong>${text}</strong></button></p>
+  </section>
+  <section>
+    <h1>Callouts</h1>
+    <div class='callouts'>
+      <z-callout>
+        <h1>Callout: Standard (Multi-line)</h1>
+        <p>
+          Chicharrones brooklyn cardigan marfa pour-over craft beer dreamcatcher cold-pressed brunch meggings. Live-edge disrupt narwhal irony neutra single-origin coffee, biodiesel before they sold out roof party venmo farm-to-table direct trade poke tousled aesthetic. Prism dreamcatcher glossier cloud bread blue bottle farm-to-table celiac chicharrones single-origin coffee salvia knausgaard raclette.
+        </p>
+      </z-callout>
+      <z-callout type="success">
+        <h1>Callout: Standard (Multi-line)</h1>
+        <p>
+          Chicharrones brooklyn cardigan marfa pour-over craft beer dreamcatcher cold-pressed brunch meggings. Live-edge disrupt narwhal irony neutra single-origin coffee, biodiesel before they sold out roof party venmo farm-to-table direct trade poke tousled aesthetic. Prism dreamcatcher glossier cloud bread blue bottle farm-to-table celiac chicharrones single-origin coffee salvia knausgaard raclette.
+        </p>
+      </z-callout>
+      <z-callout type="warning">
+        <h1>Callout: Standard (Multi-line)</h1>
+        <p>
+          Chicharrones brooklyn cardigan marfa pour-over craft beer dreamcatcher cold-pressed brunch meggings. Live-edge disrupt narwhal irony neutra single-origin coffee, biodiesel before they sold out roof party venmo farm-to-table direct trade poke tousled aesthetic. Prism dreamcatcher glossier cloud bread blue bottle farm-to-table celiac chicharrones single-origin coffee salvia knausgaard raclette.
+        </p>
+      </z-callout>
+      <z-callout type="error">
+        <h1>Callout: Standard (Multi-line)</h1>
+        <p>
+          Chicharrones brooklyn cardigan marfa pour-over craft beer dreamcatcher cold-pressed brunch meggings. Live-edge disrupt narwhal irony neutra single-origin coffee, biodiesel before they sold out roof party venmo farm-to-table direct trade poke tousled aesthetic. Prism dreamcatcher glossier cloud bread blue bottle farm-to-table celiac chicharrones single-origin coffee salvia knausgaard raclette.
+        </p>
+      </z-callout>
+    </div>
+  </section>
+`;
+
+render(indexTemplate('button'), document.body);
