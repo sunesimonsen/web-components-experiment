24b1943621d47f1e3d0995abb68471646f37dbec
Sune Simonsen <sune@we-knowhow.dk>
2017-09-10 12:03:24 +0200

    Updates

---
+++ LitHtmlElement.js
@@ -0,0 +1,27 @@
+import { render } from './vendor/lit-html/lib/lit-extended.js'
+
+export default class LitHtmlElement extends HTMLElement {
+  // Respond to attribute changes.
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this[attr] = this.getAttribute(attr)
+      this.invalidate()
+    }
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.invalidate()
+  }
+
+  invalidate() {
+    if (!this.needsRender) {
+      this.needsRender = true;
+      Promise.resolve().then(() => {
+        this.needsRender = false;
+        render(this.render(), this.shadowRoot);
+      });
+    }
+  }
+}
